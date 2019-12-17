c587ced1d2c99fdddf5e1aa782bffe239f2b4ad1
Kamila <kmilaporto1991@gmail.com>
2019-12-17 16:54:22 -0300

    adiciona link para toda de uma cerveja e css ao CardBeer

--- src/components/cardBeer.jsx
+++ src/components/cardBeer.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+
 import './cardBeer.css'
 
 export default props => (
   <div className="col-sm-4">
     <div className="card bg-white">
       <div className="img-center">
-        <img src={props.image_url} className="card-img-top icon-beer"/>
+        <img src={props.image_url} className="card-img-top icon-beer" alt={""}/>
       </div>
       <div className="card-body bg-orange">
         <h5 className="card-title">{props.name}</h5>
         <p className="card-text">{props.tagline}</p>
-        <a href="#" className="btn btn-beer">Ver detalhes</a>
+          <Link to={`/beer/${props.id}`}>
+            <div type="button" className="btn btn-beer">Detalhes</div>
+          </Link>
       </div>
     </div>
   </div>
