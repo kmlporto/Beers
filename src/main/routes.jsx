60ad1a531196df630dca00e785cf98715bb86e74
Kamila <kmilaporto1991@gmail.com>
2019-12-17 16:52:48 -0300

    Adiciona rota para pagina de detalhes da cerveja

--- src/main/routes.jsx
+++ src/main/routes.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-
-import Beers from '../components/pages/beers'
+import PageOneBeer from '../components/pages/pageOneBeer'
+import PageBeers from '../components/pages/pageBeers'
 
 export default props => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact={true} component={Beers}></Route>
+      <Route path="/" exact={true} component={PageBeers}></Route>
+      <Route path="/beer/:id" exact={true} component={PageOneBeer}></Route>
     </Switch>
   </BrowserRouter>
 
